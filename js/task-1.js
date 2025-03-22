366ada3d705c1fe5ccde1ef69b124c2861a26e39
Viktoriia_Kytsenko <vikakitsenko@gmail.com>
2025-03-22 19:36:45 +0100

    performance of tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,10 @@
 "use strict";
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
 
-  if (totalPrice > customerCredits) {
-    return "Insufficient funds!";
-  }
-
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function slugify(title) {
+  return title.toLowerCase().split(" ").join("-");
 }
 
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+console.log(slugify("My First Blog Post"));
+console.log(slugify("JavaScript Basics and Beyond"));
+console.log(slugify("Learn to Code for Free"));
+console.log(slugify("Frontend Development Tips"));

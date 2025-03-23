9acf72b7cb87de52e2822c508b426bd3bb71bf7f
Viktoriia_Kytsenko <vikakitsenko@gmail.com>
2025-03-23 11:24:50 +0100

    prettier

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,6 @@
 
 function makeArray(firstArray, secondArray, maxLength) {
   const newArray = firstArray.concat(secondArray);
-
   return newArray.length > maxLength ? newArray.slice(0, maxLength) : newArray;
 }
 
